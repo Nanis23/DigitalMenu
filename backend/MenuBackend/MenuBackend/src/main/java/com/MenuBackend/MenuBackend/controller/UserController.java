d316b76a1bce197be5a019d5fd82bd79d78a30f7
Denis <denisdemaj233@gmail>
2025-04-10 15:04:12 +0200

    Rregullimi i emrit nga AdminController ne UserController.java

--- backend/MenuBackend/MenuBackend/src/main/java/com/MenuBackend/MenuBackend/controller/UserController.java
+++ backend/MenuBackend/MenuBackend/src/main/java/com/MenuBackend/MenuBackend/controller/UserController.java
@@ -1,11 +1,4 @@
 package com.MenuBackend.MenuBackend.controller;
 
-import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequestMapping("/api/admin")
-@RequiredArgsConstructor
-public class AdminController {
+public class UserController {
 }
