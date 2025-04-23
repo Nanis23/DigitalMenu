6cbde734db4527b12d36babc5ca691e4f59846e9
denisdemaj233 <denisdemaj233@gmail.com>
2025-04-23 23:09:24 +0200

    hjfyuff

--- frontend/MenuFrontend/src/app/dashboard/user/user-component/user-component.component.ts
+++ frontend/MenuFrontend/src/app/dashboard/user/user-component/user-component.component.ts
@@ -2,20 +2,12 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from '../../shared/header/header.component';
 import { SidebarComponent } from '../../shared/sidebar/sidebar.component';
 import { FooterComponent } from '../../shared/footer/footer.component';
-<<<<<<< HEAD
 import { ReactiveFormsModule } from '@angular/forms';
-import { AddUserComponent } from '../addUser/add-user/add-user.component';
+
 
 @Component({
   selector: 'app-user-component',
-  imports: [HeaderComponent,SidebarComponent,FooterComponent,AddUserComponent,ReactiveFormsModule],
-=======
-import { RouterOutlet } from '@angular/router';
-
-@Component({
-  selector: 'app-user-component',
-  imports: [HeaderComponent, SidebarComponent, FooterComponent],
->>>>>>> 471f9b5636e57632ba780e45cc13f71678532a3f
+  imports: [HeaderComponent,SidebarComponent,FooterComponent,ReactiveFormsModule],
   templateUrl: './user-component.component.html',
   styleUrl: './user-component.component.css',
 })
