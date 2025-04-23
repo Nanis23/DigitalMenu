471f9b5636e57632ba780e45cc13f71678532a3f
Nanis23 <sinajsinan1@gmail.com>
2025-04-23 23:04:11 +0200

    Update i sidebar dhe user component html

--- frontend/MenuFrontend/src/app/dashboard/user/user-component/user-component.component.ts
+++ frontend/MenuFrontend/src/app/dashboard/user/user-component/user-component.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from '../../shared/header/header.component';
 import { SidebarComponent } from '../../shared/sidebar/sidebar.component';
 import { FooterComponent } from '../../shared/footer/footer.component';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-user-component',
-  imports: [HeaderComponent,SidebarComponent,FooterComponent],
+  imports: [HeaderComponent, SidebarComponent, FooterComponent],
   templateUrl: './user-component.component.html',
   styleUrl: './user-component.component.css',
 })
